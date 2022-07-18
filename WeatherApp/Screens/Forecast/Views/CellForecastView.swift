52830336b0059c2e92911c0a8751b8179afd616d
Dima Kozhanovski <kozhanovskidima@gamil.com>
2022-07-18 18:39:45 +0300

    was added logic in swift ui

---
+++ WeatherApp/Screens/Forecast/Views/CellForecastView.swift
@@ -0,0 +1,55 @@
+//
+//  CellForecastView.swift
+//  WeatherApp
+//
+//  Created by Dima Kozhanovsky on 01/07/2022.
+//
+
+import SwiftUI
+
+struct CellForecastView: View {
+    // let weatherPlaceHolder = UIImage(systemName: "sun")!
+    var model : ForeCastWeatherModel
+    var body: some View {
+        Spacer()
+        Text("London")
+        Image("MulticolorStripe")
+            .frame(width: 1, height: 2)
+        HStack {
+            Text("Today")
+            Spacer()
+        }
+            
+        List(1..<5) { item in
+            ZStack{
+                    HStack {
+                        Image(systemName: "cloud.rain")
+                            .previewDisplayName("Вася")
+                            .scaleEffect(2.3)
+                            .padding(34)
+                            .foregroundColor(Color.yellow)
+                        VStack {
+                            
+                            Text(String(model.list[0].current.temperatureForecast ?? 44))
+                          
+                            Text("Сlear")
+                        }
+                            Spacer()
+                        Text("22")
+                     //   Text(String(model.listforForeCast?[0].current?[0].temperatureForecast ?? 222))
+                            .padding(40)
+                            .scaleEffect(2.5)
+                            .foregroundColor(Color.blue)
+                    }
+                       
+                    Rectangle().stroke(lineWidth: 2).frame( idealWidth: .infinity,  maxHeight: 85 ).tag("1").foregroundColor(Color.blue)
+                        
+            
+        }
+            .padding(.horizontal , -15 )
+    
+}
+        .listStyle(.grouped)
+        //Making functions chainable is quite easy and can allow us to write using an almost DSL-like syntax.}
+}
+}
