dfe432e046fbd3bcf49bb4deb01ac2fdc775929a
DimaKozhanovsky <koganovski1989koganovski@gmail.com>
2023-04-25 20:53:39 +0300

    was added share feature

--- WeatherApp/Services/NetworkImages/NetworkImageView.swift
+++ WeatherApp/Services/NetworkImages/NetworkImageView.swift
@@ -10,15 +10,21 @@ import UIKit
 import Combine
 
 struct NetworkImageView<Placeholder: View, ProgressBlock: View>: View {
+    //  тут мы описваемм  2 дженрика Placeholder и ProgressBlock и подрисываем их на проткол вью чтобы  можно было засунуть все, что конформит протокол View для реюзабельности
     
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    //@Environment нужно чтобы понимать настройки юзера и под них адаптировать кожд
+    // In SwiftUI, we can switch between a HStack and VStack based on the horizontalSizeClass that we get from the @Environment:
     @StateObject var viewModel: ImageViewModel
     
     let placeholder: Placeholder
     let progressBlock: ProgressBlock
+    // в константы выше можно ложить все что подписано на протокол View
     
     init(
+        //In Swift, an initializer is a special function that we use to create objects of a particular class, struct, or enum. Initializers are sometimes called constructors because they "construct" objects.
         imageUrlString: String,
+        //
         @ViewBuilder placeholder: () -> Placeholder,
         @ViewBuilder progressBlock: () -> ProgressBlock
     ) {
